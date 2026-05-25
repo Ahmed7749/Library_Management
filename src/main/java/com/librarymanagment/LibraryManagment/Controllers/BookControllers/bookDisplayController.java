f7f85eb11a26cefcaf49b5cc0625c883cc107dc0
Ahmed7749 <ahmadkmm2@gmail.com>
2026-05-25 17:12:01 +0300

    injected book service

--- src/main/java/com/librarymanagment/LibraryManagment/Controllers/BookControllers/bookDisplayController.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Controllers/BookControllers/bookDisplayController.java
@@ -2,6 +2,7 @@ package com.librarymanagment.LibraryManagment.Controllers.BookControllers;
 
 
 import com.librarymanagment.LibraryManagment.Repostries.BookRepository;
+import com.librarymanagment.LibraryManagment.Services.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("books")
 public class bookDisplayController {
 
-    private final BookRepository bookRepository;
 
-    public bookDisplayController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    private final BookService bookService;
+
+    public bookDisplayController(BookService bookService) {
+        this.bookService = bookService;
     }
-
 
     @GetMapping("display")
     public String generateList(Model model){
-        model.addAttribute("booksList", bookRepository.findAll());
+        model.addAttribute("booksList", bookService.getAllBooks());
         return "BookView/displayBook";
     }
 }
