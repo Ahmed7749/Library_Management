16f7b07d3684dce23d3276c03f1c0aaa04d4095c
Ahmed7749 <ahmadkmm2@gmail.com>
2026-05-25 16:29:12 +0300

    implemented and included repositories of author and category to fetch the names objects with ids (a hash map would be a better solution then fetching the whole object btw)

--- src/main/java/com/librarymanagment/LibraryManagment/Controllers/BookControllers/BookCreationController.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Controllers/BookControllers/BookCreationController.java
@@ -1,7 +1,9 @@
 package com.librarymanagment.LibraryManagment.Controllers.BookControllers;
 
 import com.librarymanagment.LibraryManagment.Entities.Book;
+import com.librarymanagment.LibraryManagment.Repostries.AuthorRepository;
 import com.librarymanagment.LibraryManagment.Repostries.BookRepository;
+import com.librarymanagment.LibraryManagment.Repostries.CategoryRepository;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,24 +18,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BookCreationController {
 
     private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+    private final CategoryRepository categoryRepository;
 
-    public BookCreationController(BookRepository bookRepository) {
+    public BookCreationController(BookRepository bookRepository, AuthorRepository authorRepository, CategoryRepository categoryRepository) {
         this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+        this.categoryRepository = categoryRepository;
     }
 
 
     @GetMapping("create")
     public String bookPage(Model model){
         model.addAttribute("book", new Book());
+        model.addAttribute("authors", authorRepository.findAll());
+        model.addAttribute("categories", categoryRepository.findAll());
         return "BookView/bookCreation";
     }
 
+
     @PostMapping("create")
-    public String bookCreation(@ModelAttribute("book") @Valid Book book, BindingResult result){
+    public String bookCreation(@ModelAttribute("book") @Valid Book book, BindingResult result, Model model){
         if(result.hasErrors()){
-            return "redirect:/books/create";
+            model.addAttribute("authors", authorRepository.findAll());
+            model.addAttribute("categories", categoryRepository.findAll());
+            return "BookView/bookCreation";
         }
         bookRepository.save(book);
         return "redirect:/books/create";
     }
+
+
+
+
 }
