d8eceed4fe26d3126255cd216c0adeaa8cadf74f
Ahmed7749 <ahmadkmm2@gmail.com>
2026-05-25 16:27:23 +0300

    deleted unsued import statement

--- src/main/java/com/librarymanagment/LibraryManagment/Repostries/CategoryRepository.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Repostries/CategoryRepository.java
@@ -2,6 +2,7 @@ package com.librarymanagment.LibraryManagment.Repostries;
 
 import com.librarymanagment.LibraryManagment.Entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 
-    
-    
+
+
     Category findCategoryByName(String name);
 }
