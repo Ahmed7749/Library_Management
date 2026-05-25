c9d6128311888021d692002d607a4df1ac0b08be
Ahmed7749 <ahmadkmm2@gmail.com>
2026-05-25 14:32:34 +0300

    category repository

---
+++ src/main/java/com/librarymanagment/LibraryManagment/Repostries/CategoryRepository.java
@@ -0,0 +1,15 @@
+package com.librarymanagment.LibraryManagment.Repostries;
+
+import com.librarymanagment.LibraryManagment.Entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+    
+    
+    Category findCategoryByName(String name);
+}
