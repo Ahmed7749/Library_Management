9f5cc952fa3b4303d8121bcedbd7bc936f5484a3
Ahmed7749 <ahmadkmm2@gmail.com>
2026-05-25 17:11:52 +0300

    injected 3 classes of authorService and categoryService with book service

--- src/main/java/com/librarymanagment/LibraryManagment/Controllers/BookControllers/BookCreationController.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Controllers/BookControllers/BookCreationController.java
@@ -1,9 +1,9 @@
 package com.librarymanagment.LibraryManagment.Controllers.BookControllers;
 
 import com.librarymanagment.LibraryManagment.Entities.Book;
-import com.librarymanagment.LibraryManagment.Repostries.AuthorRepository;
-import com.librarymanagment.LibraryManagment.Repostries.BookRepository;
-import com.librarymanagment.LibraryManagment.Repostries.CategoryRepository;
+import com.librarymanagment.LibraryManagment.Services.AuthorService;
+import com.librarymanagment.LibraryManagment.Services.BookService;
+import com.librarymanagment.LibraryManagment.Services.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,22 +17,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("books")
 public class BookCreationController {
 
-    private final BookRepository bookRepository;
-    private final AuthorRepository authorRepository;
-    private final CategoryRepository categoryRepository;
 
-    public BookCreationController(BookRepository bookRepository, AuthorRepository authorRepository, CategoryRepository categoryRepository) {
-        this.bookRepository = bookRepository;
-        this.authorRepository = authorRepository;
-        this.categoryRepository = categoryRepository;
+    private final AuthorService authorService;
+    private final CategoryService categoryService;
+    private final BookService bookService;
+    public BookCreationController(AuthorService authorService, CategoryService categoryService, BookService bookService) {
+        this.authorService = authorService;
+        this.categoryService = categoryService;
+        this.bookService = bookService;
     }
-
 
     @GetMapping("create")
     public String bookPage(Model model){
         model.addAttribute("book", new Book());
-        model.addAttribute("authors", authorRepository.findAll());
-        model.addAttribute("categories", categoryRepository.findAll());
+        model.addAttribute("authors", authorService.getAllAuthors());
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "BookView/bookCreation";
     }
 
@@ -40,11 +39,11 @@ public class BookCreationController {
     @PostMapping("create")
     public String bookCreation(@ModelAttribute("book") @Valid Book book, BindingResult result, Model model){
         if(result.hasErrors()){
-            model.addAttribute("authors", authorRepository.findAll());
-            model.addAttribute("categories", categoryRepository.findAll());
+            model.addAttribute("authors", authorService.getAllAuthors());
+            model.addAttribute("categories", categoryService.getAllCategories());
             return "BookView/bookCreation";
         }
-        bookRepository.save(book);
+        bookService.saveBook(book);
         return "redirect:/books/create";
     }
 
