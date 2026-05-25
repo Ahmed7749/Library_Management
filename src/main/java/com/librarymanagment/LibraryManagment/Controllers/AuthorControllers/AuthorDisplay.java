db2bf5d86c94374cd76345c6de824b58e0e3b838
Ahmed7749 <ahmadkmm2@gmail.com>
2026-05-25 17:11:13 +0300

    injected author service instead of repository

--- src/main/java/com/librarymanagment/LibraryManagment/Controllers/AuthorControllers/AuthorDisplay.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Controllers/AuthorControllers/AuthorDisplay.java
@@ -1,6 +1,7 @@
 package com.librarymanagment.LibraryManagment.Controllers.AuthorControllers;
 
 import com.librarymanagment.LibraryManagment.Repostries.AuthorRepository;
+import com.librarymanagment.LibraryManagment.Services.AuthorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/authors")
 public class AuthorDisplay {
-    private AuthorRepository authorRepository;
-    public AuthorDisplay(AuthorRepository authorRepository){
-        this.authorRepository = authorRepository;
+
+
+    private final AuthorService authorService;
+
+    public AuthorDisplay(AuthorService authorService) {
+        this.authorService = authorService;
     }
 
     @GetMapping("/display")
     public String displayPage(Model model){
-        model.addAttribute("authorList", authorRepository.findAll());
+        model.addAttribute("authorList", authorService.getAllAuthors());
         return "AuthorView/displayAuthor";
     }
 }
