f3482a6197419b7f01f71dfb4643891593cb545f
Ahmed7749 <ahmadkmm2@gmail.com>
2026-05-25 14:29:10 +0300

    created author entity with including validations for data

---
+++ src/main/java/com/librarymanagment/LibraryManagment/Entities/Author.java
@@ -0,0 +1,59 @@
+package com.librarymanagment.LibraryManagment.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank(message = "please enter author name")
+    private String authorName;
+    @NotBlank(message = "please select nationality")
+    private String nationality;
+
+    public Author() {
+        authorName = null;
+        nationality = null;
+    }
+
+    public Author(String authorName, String nationality) {
+        this.authorName = authorName;
+        this.nationality = nationality;
+    }
+
+    public Author(long id, String authorName, String nationality) {
+        this.id = id;
+        this.authorName = authorName;
+        this.nationality = nationality;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+}
