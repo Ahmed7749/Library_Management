0781054842ed8236c42ef68fd5426704932b637b
Ahmed7749 <ahmadkmm2@gmail.com>
2026-05-25 17:12:39 +0300

    injected category service class

--- src/main/java/com/librarymanagment/LibraryManagment/Controllers/CategoryControllers/CategoryDisplay.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Controllers/CategoryControllers/CategoryDisplay.java
@@ -1,6 +1,7 @@
 package com.librarymanagment.LibraryManagment.Controllers.CategoryControllers;
 
 import com.librarymanagment.LibraryManagment.Repostries.CategoryRepository;
+import com.librarymanagment.LibraryManagment.Services.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CategoryDisplay {
 
 
-    private final CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
 
-    public CategoryDisplay(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
+    public CategoryDisplay(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
     @GetMapping("display")
     public String display(Model model){
-        model.addAttribute("categoryList", categoryRepository.findAll());
+        model.addAttribute("categoryList", categoryService.getAllCategories());
         return "CategoryView/categoryDisplay";
     }
 }
