5c0e3534f0eb2ad57bbadbbe3f1cec2a55b54d9f
Ahmed7749 <ahmadkmm2@gmail.com>
2026-06-15 12:24:49 +0300

    User pojo class to contain the user

---
+++ src/main/java/com/librarymanagment/LibraryManagment/Entities/User.java
@@ -0,0 +1,70 @@
+package com.librarymanagment.LibraryManagment.Entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 5, max = 20)
+    private String username;
+
+    @NotNull
+    @Size(min = 8)
+    private String password;
+
+    private String role;
+
+    public User(){
+        username = null;
+        password = null;
+        role = null;
+    }
+
+    public User(long id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
