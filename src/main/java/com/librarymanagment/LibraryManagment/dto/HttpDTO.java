f592a39267c5b2177850ee499b698c3eb3bb7c06
Ahmed7749 <ahmadkmm2@gmail.com>
2026-06-29 20:42:28 +0300

    simple http data transfer object so we can encapsulate the message correctly

---
+++ src/main/java/com/librarymanagment/LibraryManagment/dto/HttpDTO.java
@@ -0,0 +1,28 @@
+package com.librarymanagment.LibraryManagment.dto;
+
+
+public class HttpDTO {
+    private String message;
+    private int statusCode;
+
+    public HttpDTO(String message, int statusCode) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+}
