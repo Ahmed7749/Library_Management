b7f8846eb3d91ff3fae1fc6441ffbef2f112965e
Ahmed7749 <ahmadkmm2@gmail.com>
2026-06-30 00:00:39 +0300

    added a manual added query to delete by id

--- src/main/java/com/librarymanagment/LibraryManagment/Repostries/AuthorRepository.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Repostries/AuthorRepository.java
@@ -4,12 +4,21 @@ import com.librarymanagment.LibraryManagment.Entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     List<Author> findAuthorsByNationality(String nationality);
+
+    Optional<Author> findAuthorById(long id);
+
+    @Modifying
+    @Query("DELETE FROM Author a WHERE a.id = :id")
+    int deleteAuthorById(@Param("id") Long id);
+
 }
