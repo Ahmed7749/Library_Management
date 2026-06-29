0d702869f716a053ed50e1b9b35a0c9355cce260
Ahmed7749 <ahmadkmm2@gmail.com>
2026-06-29 20:41:56 +0300

    an exception for the global handler to work with

---
+++ src/main/java/com/librarymanagment/LibraryManagment/exception/JsonPatchProcessingException.java
@@ -0,0 +1,7 @@
+package com.librarymanagment.LibraryManagment.exception;
+
+public class JsonPatchProcessingException extends RuntimeException {
+    public JsonPatchProcessingException(String message) {
+        super(message);
+    }
+}
