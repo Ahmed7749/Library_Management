5376fdbf64b5a8b600091b863f125cef297c3220
Ahmed7749 <ahmadkmm2@gmail.com>
2026-06-30 22:59:51 +0300

    added 2 methods the getBooksBYAuthorId and the getBookby id

--- src/main/java/com/librarymanagment/LibraryManagment/Repostries/BookRepository.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Repostries/BookRepository.java
@@ -2,20 +2,27 @@ package com.librarymanagment.LibraryManagment.Repostries;
 
 import com.librarymanagment.LibraryManagment.Entities.Author;
 import com.librarymanagment.LibraryManagment.Entities.Book;
+import com.librarymanagment.LibraryManagment.dto.BookAuthorDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    @Query("SELECT new com.librarymanagment.LibraryManagment.dto.BookAuthorDTO(b.title, b.isbn, b.pageCount, b.status, a.authorName) " +
+            "FROM Book b JOIN b.author a " +
+            "WHERE a.id = :id")
+    List<BookAuthorDTO> getBooksByAuthorId(@Param("id") long id);
 
 
-    List<Book> findBooksByAuthor_AuthorName(String authorAuthorName);
+    Optional<Book> getBookById(long id);
 
 
-    Book findBooksByAddedDate(Date addedDate);
+
+
 }
