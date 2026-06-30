96e755ca19f3d4bd2311d8d0359be1af721bf9a0
Ahmed7749 <ahmadkmm2@gmail.com>
2026-06-30 23:01:12 +0300

    added 2 methods. delete by id and get category by id

--- src/main/java/com/librarymanagment/LibraryManagment/Repostries/CategoryRepository.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Repostries/CategoryRepository.java
@@ -2,10 +2,13 @@ package com.librarymanagment.LibraryManagment.Repostries;
 
 import com.librarymanagment.LibraryManagment.Entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
@@ -13,4 +16,12 @@ public interface CategoryRepository extends JpaRepository<Category,Long> {
 
 
     Category findCategoryByName(String name);
+
+
+    Optional<Category> getCategoryById(long id);
+
+
+    @Modifying
+    @Query("DELETE FROM Category c WHERE c.id = :id")
+    int deleteCategoryById(@Param("id") long id);
 }
