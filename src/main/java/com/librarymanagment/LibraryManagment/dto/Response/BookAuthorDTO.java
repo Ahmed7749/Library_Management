9871fc950a69d1cf5a150b8a3f75d42a846f4e47
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-01 01:42:12 +0300

    moved the request book author dto to a requets sub package inside dto package

--- src/main/java/com/librarymanagment/LibraryManagment/dto/Response/BookAuthorDTO.java
+++ src/main/java/com/librarymanagment/LibraryManagment/dto/Response/BookAuthorDTO.java
@@ -1,3 +1,3 @@
-package com.librarymanagment.LibraryManagment.dto;
+package com.librarymanagment.LibraryManagment.dto.Response;
 
 public record BookAuthorDTO(String bookName, String isbn, int pageCount, String status, String authorName){}
