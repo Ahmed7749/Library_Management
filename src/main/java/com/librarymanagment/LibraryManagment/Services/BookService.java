8767e5913e2bd10df69ba68b72c568c56e8893ec
Ahmed7749 <ahmadkmm2@gmail.com>
2026-06-30 23:00:33 +0300

    updated service layer class to handle the creation of book from the usage of ReuqestDTOBOok

--- src/main/java/com/librarymanagment/LibraryManagment/Services/BookService.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Services/BookService.java
@@ -1,8 +1,15 @@
 package com.librarymanagment.LibraryManagment.Services;
 
+import com.librarymanagment.LibraryManagment.Entities.Author;
 import com.librarymanagment.LibraryManagment.Entities.Book;
+import com.librarymanagment.LibraryManagment.Entities.Category;
 import com.librarymanagment.LibraryManagment.Repostries.BookRepository;
+import com.librarymanagment.LibraryManagment.dto.BookAuthorDTO;
+import com.librarymanagment.LibraryManagment.dto.BookRequestDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,9 +17,12 @@ import java.util.List;
 public class BookService {
 
     private final BookRepository bookRepository;
-
-    public BookService(BookRepository bookRepository) {
+    private final AuthorService authorService;
+    private final CategoryService categoryService;
+    public BookService(BookRepository bookRepository, AuthorService authorService, CategoryService categoryService) {
         this.bookRepository = bookRepository;
+        this.authorService = authorService;
+        this.categoryService = categoryService;
     }
 
 
@@ -20,7 +30,36 @@ public class BookService {
         return bookRepository.findAll();
     }
 
-    public void saveBook(Book book){
-        bookRepository.save(book);
+    @Transactional
+    public Book saveBook(Book book){
+        return bookRepository.save(book);
+    }
+
+
+    @Transactional
+    public List<BookAuthorDTO> getBooksForAuthor(long id){
+        return bookRepository.getBooksByAuthorId(id);
+    }
+
+    @Transactional
+    public Book getBookById(long id){
+        return bookRepository.getBookById(id).orElseThrow( () -> new EntityNotFoundException("Book has not been found "));
+    }
+
+
+    @Transactional
+    public Book createBook(BookRequestDTO dto){
+        Author author = authorService.findById(dto.authorId());
+        Category category = categoryService.getCategoryById(dto.categoryId());
+
+        Book newBook = new Book();
+        newBook.setTitle(dto.title());
+        newBook.setIsbn(dto.isbn());
+        newBook.setPageCount(dto.pageCount());
+        newBook.setStatus(dto.status());
+        newBook.setAuthor(author);
+        newBook.setCategory(category);
+
+        return bookRepository.save(newBook);
     }
 }
