7dfcb7adf19bd0d0b599d2671dbf612e0cdf1af2
Ahmed7749 <ahmadkmm2@gmail.com>
2026-06-30 23:01:47 +0300

    included an exception handler for the error when the validation rules are not met.

--- src/main/java/com/librarymanagment/LibraryManagment/exception/GlobalExceptionHandler.java
+++ src/main/java/com/librarymanagment/LibraryManagment/exception/GlobalExceptionHandler.java
@@ -4,8 +4,13 @@ import com.librarymanagment.LibraryManagment.dto.HttpDTO;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,5 +28,21 @@ public class GlobalExceptionHandler {
         HttpDTO error = new HttpDTO(exc.getMessage(), HttpStatus.BAD_REQUEST.value());
 
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        }
+
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
